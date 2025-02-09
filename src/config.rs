ddd730d612fcac194c02b6d499850c273b09f446
yunxi20241 <liuyoufa666@gmail.com>
2025-02-09 18:20:49 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,17 +97,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new(match option_env!("RENDEZVOUS_SERVER") {
-    Some(key) if !key.is_empty() => key,
-    _ => "",
-}.to_owned());
-// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 
-pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
-    Some(key) if !key.is_empty() => key,
-    _ => PUBLIC_RS_PUB_KEY,
-};
-// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.yunxi6.top"];
+pub const RS_PUB_KEY: &str = "18370754048";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
