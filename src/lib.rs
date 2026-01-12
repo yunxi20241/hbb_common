a40c0448a134ad55334e512f168ff3c79c28a7f7
yunxilyf <2574379425@qq.com>
2026-01-12 11:25:30 +0800

    Update version check URL to use fork repository

--- src/lib.rs
+++ src/lib.rs
@@ -435,7 +435,7 @@ pub const VER_TYPE_RUSTDESK_CLIENT: &str = "rustdesk-client";
 pub const VER_TYPE_RUSTDESK_SERVER: &str = "rustdesk-server";
 
 pub fn version_check_request(typ: String) -> (VersionCheckRequest, String) {
-    const URL: &str = "https://api.rustdesk.com/version/latest";
+    const URL: &str = "https://api.github.com/repos/yunxi20241/rustdesk1/releases/latest";
 
     use sysinfo::System;
     let system = System::new();
